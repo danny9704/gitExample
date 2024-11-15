3c69a481ae6edb46f199c816f111b09dcdecbefa
Jo SeongJae <danny9704@pusan.ac.kr>
2024-11-15 18:16:41 +0900

    modified:   applications/app1/src/appApp1.F90 	modified:   applications/app1/src/glbApp1.F90

--- applications/app1/src/appApp1.F90
+++ applications/app1/src/appApp1.F90
@@ -5,19 +5,7 @@ Program app1
 !! -------------------------------------------------------------------------- !!
 Implicit None
 !! -------------------------------------------------------------------------- !!
-    Type(typShape) :: shape1
-    Character(len=:), Allocatable :: shape1Color
-    Logical :: shape1Filled
-
-    shape1%color = "red"
-    shape1%filled = .TRUE.
-
-    Call shape1%GetColor( shape1Color )
-    shape1Filled = shape1%IsFilled()
-
-    write(*,*) "Shape1 Color : ", shape1Color
-    write(*,*) "Shape1 Filled: ", shape1Filled
-
+    
 !! -------------------------------------------------------------------------- !!
 End Program
 !! -------------------------------------------------------------------------- !!
