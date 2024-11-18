f835bf75d15c77f3f3fd4c5dbe67370b975aed93
Jo SeongJae <danny9704@pusan.ac.kr>
2024-11-18 17:50:22 +0900

    modified:   applications/app1/src/glbApp1.F90

--- applications/app1/src/glbApp1.F90
+++ applications/app1/src/glbApp1.F90
@@ -7,28 +7,29 @@ Implicit None
 !! -------------------------------------------------------------------------- !!
 
 module convert_library
-    implicit none
- contains
-    subroutine convert_pat_to_hst(input_file, output_file)
-       implicit none
-       character(len=*), intent(in) :: input_file, output_file
-       integer :: iounit_in, iounit_out, i
-       character(len=256) :: line
- 
-       open(unit=iounit_in, file=input_file, status='old', action='read')
-       open(unit=iounit_out, file=output_file, status='replace', action='write')
- 
-       do
-          read(iounit_in, '(A)', iostat=i) line
-          if (i /= 0) exit  
-          write(iounit_out, '(A)') trim(line)
-       end do
- 
+   implicit none
+contains
+   subroutine convert_pat_to_hst()
+      implicit none
+      integer :: iounit_in, iounit_out, i
+      character(len=256) :: line
+      character(len=256), parameter :: input_file = 'Semi.pat'
+      character(len=256), parameter :: output_file = 'Semi.hst'
 
-       close(iounit_in)
-       close(iounit_out)
-    end subroutine convert_pat_to_hst
- end module convert_library
+      open(unit=iounit_in, file=input_file, status='old', action='read')
+      open(unit=iounit_out, file=output_file, status='replace', action='write')
+
+      do
+         read(iounit_in, '(A)', iostat=i) line
+         if (i /= 0) exit
+         write(iounit_out, '(A)') trim(line)
+      end do
+
+      close(iounit_in)
+      close(iounit_out)
+   end subroutine convert_pat_to_hst
+end module convert_library
+
     
 
 !! -------------------------------------------------------------------------- !!
